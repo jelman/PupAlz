6b37bbbca1d4f265615d27cbb1b52b79a4e0240e
jelman <jelman523@gmail.com>
2019-05-23 16:06:43 -0700

    finish first draft of HVLT recall script

--- pupillometry/hvlt_recall_proc_subject.py
+++ pupillometry/hvlt_recall_proc_subject.py
@@ -37,65 +37,23 @@ except ImportError:
 
 def plot_trials(pupildf, fname):
     sns.set_style("ticks")
-    palette = sns.color_palette("deep", n_colors=len(pupildf.Trial.unique()))
-    p = sns.lineplot(data=pupildf, x="Timestamp",y="Dilation", hue="Trial", palette=palette, legend="brief")
-    plt.xticks(rotation=45)
+    p = sns.lineplot(data=pupildf, x="Timestamp",y="Dilation")
     plt.tight_layout()
-    plt.legend(loc='best')
     plot_outname = pupil_utils.get_proc_outfile(fname, "_PupilPlot.png")
+    plot_outname = plot_outname.replace("-Recognition","")
     p.figure.savefig(plot_outname)
     plt.close()
     
     
-def clean_trials(df, trialevents):
-    resampled_dict = {}
-    for trialnum in trialevents.Trial.unique():
-        basestart, basestop, respstart, respstop =  trialevents.loc[trialevents.Trial==trialnum,'TETTime']
-        condition = trialevents.loc[trialevents.Trial==trialnum,'Condition'].iat[0]
-        rawtrial = df.loc[(df.TETTime>=basestart) & (df.TETTime<=respstop)]
-        rawtrial.loc[(rawtrial.TETTime>=basestart) & (rawtrial.TETTime<=basestop),'Phase'] = 'Baseline' 
-        rawtrial.loc[(rawtrial.TETTime>=respstart) & (rawtrial.TETTime<=respstop),'Phase'] = 'Response' 
-#        rawtrial = rawtrial[rawtrial.Condition=='Response']
-        cleantrial = pupil_utils.deblink(rawtrial)
-        trial_resamp = pupil_utils.resamp_filt_data(cleantrial, filt_type='low', string_cols=['CurrentObject', 'Phase'])
-        baseline = trial_resamp['DiameterPupilLRFilt'].first('2000ms').mean()
-#        baseline = trial_resamp.DiameterPupilLRFilt.iat[0]
-        trial_resamp['Baseline'] = baseline
-        trial_resamp['Dilation'] = trial_resamp['DiameterPupilLRFilt'] - trial_resamp['Baseline']
-        trial_resamp = trial_resamp[trial_resamp.Phase=='Response']
-        trial_resamp['Condition'] = condition
-        resampled_dict[trialnum] = trial_resamp        
-    dfresamp = pd.concat(resampled_dict, names=['Trial','Timestamp'], sort=True)
-    return dfresamp
-    
-
-def get_trial_events(df):
-    """
-    Create dataframe of trial events. This includes:
-        Condition: ['Letter', 'Category']
-        Trial: [1, 2, 3, 4, 5, 6]
-        TrialPhase = ['Baseline', 'Response']
-        StartStop = ['Start', 'Stop']
-    First finds timestamps where CurrentObject changes to determine starts and stops.
-    Combines these and defines trial, phase and whether it is start or stop time. 
-    """
-    startidx = df['CurrentObject'].ne(df['CurrentObject'].shift().ffill()).astype(bool)
-    stopidx = df['CurrentObject'].ne(df['CurrentObject'].shift(-1).bfill()).astype(bool)
-    trialevents_start = pd.DataFrame(df.loc[startidx, ['TETTime','CurrentObject']])
-    trialevents_stop = pd.DataFrame(df.loc[stopidx, ['TETTime','CurrentObject']])
-    trialevents_start = trialevents_start.loc[(trialevents_start.CurrentObject=="ReadLetter") | 
-                                              (trialevents_start.CurrentObject == "RecordLetter")]
-    trialevents_stop = trialevents_stop.loc[(trialevents_stop.CurrentObject=="BeginFile") | 
-                                            (trialevents_stop.CurrentObject == "RecordLetter")]
-    trialevents_start['TrialPhase'] = np.tile(['Baseline','Response'], 6)
-    trialevents_start['StatStop'] = 'Start'
-    trialevents_stop['TrialPhase'] = np.tile(['Baseline','Response'], 6)
-    trialevents_stop['StartStop'] = 'Stop'
-    trialevents = trialevents_start.append(trialevents_stop).sort_index()
-    trialevents['Trial'] = np.repeat(range(1,7), 4)
-    trialevents['Condition'] = np.repeat(['Letter', 'Category'], 12)
-    return trialevents
-
+def clean_trials(df):
+        dfresamp = pupil_utils.resamp_filt_data(df, filt_type='low', string_cols=['CurrentObject'])
+        baseline = dfresamp['DiameterPupilLRFilt'].first('2000ms').mean()
+        dfresamp['Baseline'] = baseline
+        dfresamp['Dilation'] = dfresamp['DiameterPupilLRFilt'] - dfresamp['Baseline']
+        onset_time = dfresamp.CurrentObject.ne('Recallinstructions').idxmax()
+        dfresamp.index = dfresamp.index - onset_time
+        return dfresamp
+        
    
 def proc_subject(filelist):
     """Given an infile of raw pupil data, saves out:
@@ -113,20 +71,20 @@ def proc_subject(filelist):
             raise IOError('Could not open {}'.format(fname))
         df = df[df.CurrentObject.str.contains("Recall", na=False)]
         df = pupil_utils.deblink(df)
-        dfresamp = pupil_utils.resamp_filt_data(df, filt_type='low', string_cols=['CurrentObject'])
-        
-        
-        dfresamp = clean_trials(df, trialevents)
-        dfresamp = dfresamp.reset_index(drop=False).set_index(['Condition','Trial'])
-        dfresamp['Timestamp'] = dfresamp.groupby(level='Trial')['Timestamp'].transform(lambda x: x - x.iat[0])
-        dfresamp1s = dfresamp.groupby(level=['Condition','Trial']).apply(lambda x: x.resample('1S', on='Timestamp', closed='right', label='right').mean())
-        pupilcols = ['Subject', 'Trial', 'Condition', 'Timestamp', 'Dilation',
-                     'Baseline', 'DiameterPupilLRFilt', 'BlinksLR']
-        pupildf = dfresamp1s.reset_index()[pupilcols].sort_values(by=['Trial','Timestamp'])
-        pupildf = pupildf[pupilcols].rename(columns={'DiameterPupilLRFilt':'Diameter',
-                                         'BlinksLR':'BlinkPct'})
-        pupildf['Timestamp'] = pd.to_datetime(pupildf.Timestamp).dt.strftime('%H:%M:%S')
+        dfresamp = clean_trials(df)
+        dfresamp1s = dfresamp.resample('1S', closed='right', label='right').mean()
+        dfresamp1s.index = dfresamp1s.index.round('S')
+        dfresamp1s = dfresamp1s.dropna(how='all')
+        pupildf = dfresamp1s.reset_index().rename(columns={
+                                            'index':'Timestamp',
+                                            'DiameterPupilLRFilt':'Diameter',
+                                            'BlinksLR':'BlinkPct'})
+        pupilcols = ['Subject', 'Timestamp', 'Dilation',
+                     'Baseline', 'Diameter', 'BlinkPct']
+        pupildf = pupildf[pupilcols].sort_values(by='Timestamp')
+        pupildf['Timestamp'] = pupildf.Timestamp.dt.total_seconds()
         pupil_outname = pupil_utils.get_proc_outfile(fname, '_ProcessedPupil.csv')
+        pupil_outname = pupil_outname.replace("-Recognition","")
         pupildf.to_csv(pupil_outname, index=False)
         plot_trials(pupildf, fname)
 
